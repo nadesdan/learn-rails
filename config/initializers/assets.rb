bc91a054253ed76c0008c8c6a039b21c922039ba
Daniel Benjamin <nadesdan@gmail.com>
2014-09-15 14:35:59 +0200

    update application layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+LearnRails::Application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# LearnRails::Application.config.assets.precompile += %w( search.js )
